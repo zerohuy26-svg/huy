cc4527bb77f4b172fd678f0b7301dadbfd096b1e
zerohuy26-svg <zerohuy26@gmail.com>
2025-12-03 23:59:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Anh iu bé ,Anh hứa hongg làm bé buồn nữa đouuu :3";
+  titleElement.innerHTML = "Em thưn bé Yến ,Em hứa hongg làm bé buồn nữa đouuu :3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -43,11 +43,11 @@ function resizeYesButton() {
 function generateMessage(noCount) {
   const messages = [
     "Không Bao Giờ",
-    "Anh bicc lỗi rồi ạa",
-    "Mong bé tha lỗi choo anhh :((",
-    "Anhh saii rồi , anhh đáng trách ạ",
-    "Bé đừng giận anhh nữa nhoo",
-    "Anhhh iu bé nhắm nhunnn đóoooo",
+    "Emm bicc lỗi rồi ạa",
+    "Mong bé tha lỗi choo emm:< ",
+    "Emm saii rồi , Emm đáng trách ạ",
+    "Bé đừng giận em nữa nhoo :3",
+    "Em thưn bé Yến nhắm nhunnn đóoooo",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
